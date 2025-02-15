321c7bea26d258768160b8710286b22a49c182fe
Belouahed <100034909+Belouahed@users.noreply.github.com>
2025-02-15 18:16:03 +0100

    update

--- src/routes/Home/home.jsx
+++ src/routes/Home/home.jsx
@@ -4,7 +4,7 @@ function home() {
   return (
     <>
       <h1>Home</h1>
-      <p>Noureddine + Belouahed.</p>
+      <p>Noureddine + Belouahed React js.</p>
       <p>It uses NodeJs, Express & MongoDB as a backend!</p>
     </>
   );
